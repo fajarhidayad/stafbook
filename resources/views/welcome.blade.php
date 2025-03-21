d352c1d5b4b3ba056ba67e00111303f56a970ab8
Fajar Hidayad <fajarsuryahidayad@gmail.com>
2025-03-21 14:28:53 +0700

    update landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,21 @@
 <x-layout>
 
-    <h1>Hello</h1>
+    <div style="min-height: 80vh" class="d-flex flex-column align-items-center justify-content-center">
+        <h1>Prod.ly</h1>
+        <p>Product management</p>
+
+        @guest
+            <div class="d-flex">
+                <a class="btn btn-primary me-3" href="{{ route('show.login') }}">Login</a>
+                <a class="btn btn-outline-primary" href="{{ route('show.register') }}">Register</a>
+            </div>
+        @endguest
+        @auth
+            <div class="d-flex">
+                <a class="btn btn-primary me-3" href="{{ route('products.index') }}">Products</a>
+                <a class="btn btn-outline-primary" href="{{ route('categories.index') }}">Categories</a>
+            </div>
+        @endauth
+    </div>
 
 </x-layout>
